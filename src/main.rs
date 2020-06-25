9872c92801908390c9d85071b0225be2691b0139
Oluwapelumi Ajayi <opelajayi@yahoo.com>
2020-06-25 22:43:48 +0100

    Finish plain connection server

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,97 @@
-use std::io::prelude::*;
-use std::net::TcpListener;
-use std::net::TcpStream;
-use std::thread;
+use tokio::prelude::*;
+use tokio::net::TcpListener;
+use tokio::net::TcpStream;
 
 use i8583::Unpacker;
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:5001").unwrap();
+#[tokio::main]
+async fn main() {
+    let mut listener = TcpListener::bind("0.0.0.0:5003").await.unwrap();
 
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
+    loop {
+        let (socket, _) = match listener.accept().await {
+            Ok(val) => val,
+            Err(e) => {
+                eprintln!("Accept Error: {}", e);
+                continue;
+            }
+        };
 
-        thread::spawn(|| {
-            handle_connection(stream);
+        tokio::spawn( async move {
+            // Process each socket concurrently.
+            handle_connection(socket).await
         });
     }
 }
 
-fn handle_connection(mut stream: TcpStream) {
+async fn handle_connection(mut stream: TcpStream) {
     
     // Read request
     // let mut request = Vec::with_capacity(1024);
     let mut request = [0; 4096];
 
-    if let Err(e) = stream.read(&mut request) {
-        panic!("encountered IO error: {}", e)
-    };
-    println!("bytes: {}", std::str::from_utf8(&request).unwrap());
-
-    // Unpack and log
-    let mut unpacker = Unpacker::new(&request[2..]);
-    let fields = unpacker.unpack(&i8583::nibss::SPEC).unwrap();
-    for (i, field) in fields.iter().enumerate() {
-        if let Some(val) = field {
-            println!("[{:03}] {}", i, std::str::from_utf8(val).unwrap());
+    let _n = match stream.read(&mut request).await {
+        // socket closed
+        Ok(n) if n == 0 => return,
+        Ok(n) => n,
+        Err(e) => {
+            eprintln!("failed to read from socket; err = {:?}", e);
+            return;
         }
-    }
+    };
+
+    unpack_and_log(&request);
 
     // Forward message and receive response
-    let mut destination = TcpStream::connect("197.253.19.75:5003").unwrap();
-    println!("Connected to the server!");
-    destination.write(&request).unwrap();
+    let mut destination = match TcpStream::connect("197.253.19.75:5003").await {
+        Ok(destination) => destination,
+        Err(e) => {
+            eprintln!("Could not connect to destination server: {}", e);
+            return;
+        }
+    };
+
+    if let Err(e) = destination.write(&request).await {
+        eprintln!("Could not write to destination server: {}", e);
+        return;
+    }
+
     let mut response = [0; 4096];
-    destination.read(&mut response).unwrap();
+    if let Err(e) = destination.read(&mut response).await {
+        eprintln!("Could not read from destination server: {}", e);
+        return;
+    }
+
+    unpack_and_log(&response);
 
     // Return response to client
-    stream.write(&response).unwrap();
-    stream.flush().unwrap();
+    if let Err(e) = stream.write(&response).await {
+        eprintln!("Could not write to client: {}", e);
+        return;
+    }
+
+    if let Err(e) = stream.flush().await {
+        eprintln!("Could not flush stream: {}", e);
+        return;
+    }
+}
+
+fn unpack_and_log(iso: &[u8]) {
+    println!("bytes: {}", String::from_utf8_lossy(&iso));
+
+    // Unpack and log
+    let mut unpacker = Unpacker::new(&iso[2..]);
+    let fields = match unpacker.unpack(&i8583::nibss::SPEC) {
+        Ok(fields) => fields,
+        Err(e) => {
+            eprintln!("Unpack error: {}", e);
+            return;
+        }
+    };
+    
+    for (i, field) in fields.iter().enumerate() {
+        if let Some(val) = field {
+            println!("[{:03}] {}", i, String::from_utf8_lossy(val));
+        }
+    }
 }
